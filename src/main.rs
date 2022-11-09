824da1541ce858b98b7bacd92133cf1ac8dd98c6
Pizzakeitto <henri.lindqvist@hotmail.com>
2022-11-09 14:05:15 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+// (Mostly) written by Pizzakeitto
+
+use dotenv::dotenv;
+use std::env;
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
+
+#[group]
+#[commands(pinko, muro)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = std::env::var("TOKEN").expect("lol");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+        
+    println!("MORO SAMPPI");
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    } else {
+        println!("MORO SAMPPI");
+    }
+}
+
+#[command]
+async fn pinko(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn muro(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "MURO").await?;
+
+    Ok(())
 }
